371a2f3640cbe95fa6903950de0494ed184250d4
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-07-10 17:53:46 +0800

    nav now updates page

--- index.js
+++ index.js
@@ -4,16 +4,28 @@ const md = new Remarkable({
 // document.getElementById("markdown").innerHTML = md.render('# tada');
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "dot32.md");
+xhr.open("GET", "Dot32.md");
 xhr.onload = function()
 {
   var text = xhr.responseText;
   // console.log(text);
-  document.getElementById("markdown").innerHTML = md.render(text);
+  document.getElementsByTagName("main").item(0).innerHTML = md.render(text);
 }
 xhr.send();
 
-var nodelist = document.getElementsByTagName("button")
-for (let i = 0; i < nodelist.length; i++) {
-  console.log(nodelist.item(i).innerHTML) // .onclick = function() {}
+// var nodelist = document.getElementsByTagName("button")
+// for (let i = 0; i < nodelist.length; i++) {
+//   console.log(nodelist.item(i).innerHTML) // .onclick = function() {}
+// }
+function Dot32() {
+	xhr.open("GET", "Dot32.md");
+	xhr.send();
+}
+function Projects() {
+	xhr.open("GET", "Projects.md");
+	xhr.send();
+}
+function Posts() {
+	xhr.open("GET", "Posts.md");
+	xhr.send();
 }
