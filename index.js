1c84382fc82654b85f713f03d85d72b6bf75e28e
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-07-15 12:55:01 +0800

    console.log the requested page

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const md = new Remarkable({
 	html: true
 });
-// document.getElementById("markdown").innerHTML = md.render('# tada');
-// var page = "Dot32.md"
-// if (window.location.pathname === "/tutorials" || window.location.pathname === "/tutorials/" || window.location.pathname === "/tutorials/index.html") {
-// 	page = "Tutorials.md"
-// 	document.getElementsByTagName("main").item(0).innerHTML = "these tutorials are call amirite"
-// }
-// var page = window.location.pathname.replace('index.html','').replace('.html','')
-// console.log(page)
-// if (page.charAt(page.length-1) === "/") {
-// 	page.slice(0, -1);
-// }
-// page = page + ".md"
-// if (page === "/.md") {
-// 	page = "Dot32.md"
-// }
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", detectPageFromURL());
+console.log(detectPageFromURL())
 xhr.onload = function()
 {
   var text = xhr.responseText;
@@ -27,11 +13,6 @@ xhr.onload = function()
   hljs.highlightAll()
 }
 xhr.send();
-
-// var nodelist = document.getElementsByTagName("button")
-// for (let i = 0; i < nodelist.length; i++) {
-//   console.log(nodelist.item(i).innerHTML) // .onclick = function() {}
-// }
 
 function setContent(name) {
 	xhr.open("GET", "/" + name + ".md");
