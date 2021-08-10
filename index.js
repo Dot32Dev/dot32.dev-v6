1a508f99f53ed5a8037c1f97ffa53f239f3b3988
Dot32 <61964090+Dot32IsCool@users.noreply.github.com>
2021-08-10 12:18:58 +0800

    fix description and log image

--- index.js
+++ index.js
@@ -98,11 +98,12 @@ function getPageData() {
 
 	if (data.description) {
 		document.querySelector('meta[name="description"]').setAttribute("content", data.description);
-		document.querySelector('meta[property="og:description"]').setAttribute("content", cardTitle);
+		document.querySelector('meta[property="og:description"]').setAttribute("content", data.description);
 		console.log(data.description)
 	}
 
 	if (data.image) {
 		document.querySelector('meta[property="og:image"]').setAttribute("content", `https://dot32.netlify.app${data.image}`);
+		console.log(`https://dot32.netlify.app${data.image}`)
 	}
 }
